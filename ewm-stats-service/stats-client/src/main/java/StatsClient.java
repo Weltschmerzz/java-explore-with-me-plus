fa919b3598947c43a2a5810169d05032873cdec2
Marina Shtrassheim <mf.shtrasgeym@mail.ru>
2026-01-18 20:33:52 +0500

    stats client

---
+++ ewm-stats-service/stats-client/src/main/java/StatsClient.java
@@ -0,0 +1,55 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StatsClient extends BaseClient {
+
+    private static final String API_PREFIX = "";
+
+    @Autowired
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> saveHit(HitDto hitDto) {
+        return post("/hit", hitDto);
+    }
+
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end,
+                                           List<String> uris, Boolean unique) {
+        return get("/stats", null, createParameters(start, end, uris, unique));
+    }
+
+    private Map<String, Object> createParameters(LocalDateTime start, LocalDateTime end,
+                                                 List<String> uris, Boolean unique) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        parameters.put("end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        if (uris != null && !uris.isEmpty()) {
+            parameters.put("uris", uris.toArray());
+        }
+
+        if (unique != null) {
+            parameters.put("unique", unique);
+        }
+
+        return parameters;
+    }
+}
