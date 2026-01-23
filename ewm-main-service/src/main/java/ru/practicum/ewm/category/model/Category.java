a20de5ea0460d11b05b377973731355f8f947eed
adenosyl <rustem.kushtayev@gmail.com>
2026-01-23 17:26:57 +0500

    Фикс по тестам + убрал дубляжи

--- ewm-main-service/src/main/java/ru/practicum/ewm/category/model/Category.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/category/model/Category.java
@@ -14,15 +14,7 @@ import lombok.Setter;
 public class Category {
 
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "category_seq"
-    )
-    @SequenceGenerator(
-            name = "category_seq",
-            sequenceName = "category_seq",
-            allocationSize = 1
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 50)
